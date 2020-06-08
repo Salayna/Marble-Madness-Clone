9b2eeb0ceb11c31ca1db0825eb272d13ced9c28f
Harlequin <salaynadoukoure@gmail.com>
2020-06-08 17:15:20 +0200

    Fisrt Part of the project hanfling character movments

---
+++ Assets/Script/MovingSphere.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MovingSphere : MonoBehaviour {
+    [SerializeField, Range(0f, 100f)]
+	 float maxSpeed = 10f;
+	void Update () {
+        
+		Vector2 playerInput;
+        /*
+        Get Input axis for movement
+        */
+		playerInput.x = Input.GetAxis("Horizontal");
+		playerInput.y = Input.GetAxis("Vertical");
+       // playerInput.Normalize();
+        playerInput= Vector2.ClampMagnitude(playerInput, 1f);
+
+        //BASIC FORMULA Vitesse = Distance * Temps
+				Vector3 velocity =
+			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		Vector3 displacement = velocity * Time.deltaTime;
+		transform.localPosition += displacement;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Marble Madness clone
+
+This "Game" is a clone of marble Madness made with Unity, it is a good starter project so feel free to take a look at it
