744c5d29d996a92c7b2293112a4d58590b2c2cab
Harlequin <salaynadoukoure@gmail.com>
2020-06-08 17:35:23 +0200

    WIP controlling velocity and control

--- Assets/Script/MovingSphere.cs
+++ Assets/Script/MovingSphere.cs
@@ -2,22 +2,27 @@
 
 public class MovingSphere : MonoBehaviour {
     [SerializeField, Range(0f, 100f)]
-	 float maxSpeed = 10f;
+	 float maxAcceleration = 10f;
+     float maxSpeed = 10f;
 	void Update () {
         
 		Vector2 playerInput;
+        Vector3 velocity;
         /*
         Get Input axis for movement
         */
 		playerInput.x = Input.GetAxis("Horizontal");
 		playerInput.y = Input.GetAxis("Vertical");
        // playerInput.Normalize();
-        playerInput= Vector2.ClampMagnitude(playerInput, 1f);
+        //playerInput= Vector2.ClampMagnitude(playerInput, 1f);
+        
+		Vector3 desiredVelocity = 
+        new Vector3 (playerInput.x, 0f, playerInput.y);
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x =
+			Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+		velocity.z =
+			Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
-        //BASIC FORMULA Vitesse = Distance * Temps
-				Vector3 velocity =
-			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-		Vector3 displacement = velocity * Time.deltaTime;
-		transform.localPosition += displacement;
 	}
 }
